de1ceaffe03359a8d5ac3737b805f19ccdf140b5
felipe-apio-Souza <felipeapiodesouza@hotmail.com>
2024-11-10 01:15:59 -0300

    Adicionada formatação de tempo de execução para minutos e segundos

--- multiplicacaoMatrix.cpp
+++ multiplicacaoMatrix.cpp
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <omp.h> 
-
+#include <iomanip> // Para formatar a saída
 using namespace std;
 void inserirDados(int **matrix, int linha, int coluna);
 void printMatrix(int **matrix, int linha, int coluna);
@@ -14,44 +14,60 @@ void iniciarZerada(int** matrix, int linha, int coluna);
 int main()
 {
     double tempoSequencial, tempoParalelizado;
-    const int linhaA = 1000;
-    const int colunaA = 1000;
-    const int linhaB = 1000;
-    const int colunaB = 1000;
+     int linhaA = 1000;
+     int colunaA = 1000;
+     int linhaB = 1000;
+     int colunaB = 1000;
 
-    int** matrixA = criarMatrixDinamica(linhaA, colunaA);
-    int** matrixB = criarMatrixDinamica(linhaB, colunaB);
-    int** matrixC = criarMatrixDinamica(linhaA, colunaB);
 
-    //cout << "Matrix A:" << endl;
-    inserirDados(matrixA, linhaA, colunaA);
-    //printMatrix(matrixA, linhaA, colunaA);
-    //cout << "Matrix B:" << endl;
-    inserirDados(matrixB, linhaB, colunaB);
-    //printMatrix(matrixB, linhaB, colunaB);
-    
-    //cout << "Matrix C Sequencial:" << endl;
-    multiplicacaoMatrixSequencial(matrixA, matrixB, matrixC, linhaA, colunaA, linhaB, colunaB, tempoSequencial);
-    //printMatrix(matrixC, linhaA, colunaB);
-    
-    //cout << "Matrix C Paralelizada:" << endl;
-    multiplicacaoMatrixParallel(matrixA, matrixB, matrixC, linhaA, colunaA, linhaB, colunaB, tempoParalelizado);
-    //printMatrix(matrixC, linhaA, colunaB);
+    if(colunaA == linhaB){
+        int** matrixA = criarMatrixDinamica(linhaA, colunaA);
+        int** matrixB = criarMatrixDinamica(linhaB, colunaB);
+        int** matrixC = criarMatrixDinamica(linhaA, colunaB);
 
-    cout << "--Resultados Obtiditos--" << endl;
-    cout << "Matrix C Sequencial: " << tempoSequencial << endl;
-    cout << "Matrix C Paralelizada: " << tempoParalelizado << endl;
-    //1 / (b + (1-b)/n)
-    double aceleracaoObtida = tempoSequencial / tempoParalelizado;
-    //estou calculando com base no numero de cores do meu processador altere para o seu
-    //double eficiencia = (aceleracaoObtida / 6) * 100;
+
+        //cout << "Matrix A:" << endl;
+        inserirDados(matrixA, linhaA, colunaA);
+        //printMatrix(matrixA, linhaA, colunaA);
+        //cout << "Matrix B:" << endl;
+        inserirDados(matrixB, linhaB, colunaB);
+        //printMatrix(matrixB, linhaB, colunaB);
+        
+        //cout << "Matrix C Sequencial:" << endl;
+        multiplicacaoMatrixSequencial(matrixA, matrixB, matrixC, linhaA, colunaA, linhaB, colunaB, tempoSequencial);
+        //printMatrix(matrixC, linhaA, colunaB);
+        
+        //cout << "Matrix C Paralelizada:" << endl;
+        multiplicacaoMatrixParallel(matrixA, matrixB, matrixC, linhaA, colunaA, linhaB, colunaB, tempoParalelizado);
+        //printMatrix(matrixC, linhaA, colunaB);
+
+        cout << "--Resultados Obtiditos--" << endl;
+        
+        
+        
+        double aceleracaoObtida = tempoSequencial / tempoParalelizado;
+        
+        
+        
+
+        cout << fixed << setprecision(3); 
+        cout << "Com Sequencial   " << fixed << setprecision(3) 
+        << (int)(tempoSequencial / 60) << "m " 
+        << (int)(tempoSequencial) % 60 << "s" << endl;
+
+        cout << "Com Paralelizada " << fixed << setprecision(3) 
+        << (int)(tempoParalelizado / 60) << "m " 
+        << (int)(tempoParalelizado) % 60 << "s" << endl;
+
+        cout << aceleracaoObtida << " vezes mais rapido que a sequencial" << endl;
+        
+        deletaMatrixDinamica(matrixA, linhaA);
+        deletaMatrixDinamica(matrixB, linhaB);
+        deletaMatrixDinamica(matrixC, linhaA);
+    }else{
+        cout << "nao atende aos requisitos para realizar a multiplicacao" << endl;
+    }
     
-    cout << aceleracaoObtida << " vezes mais rapido que a sequencial" << endl;
-    //cout << eficiencia << "% do potencial paralelo" << endl;
-    
-    deletaMatrixDinamica(matrixA, linhaA);
-    deletaMatrixDinamica(matrixB, linhaB);
-    deletaMatrixDinamica(matrixC, linhaA);
     return 0;
 }
 
@@ -89,43 +105,43 @@ void printMatrix(int **matrix, int linha, int coluna){
 }
 
 void multiplicacaoMatrixSequencial(int **matrixA, int **matrixB, int **matrixC, int linhaA, int colunaA, int linhaB, int colunaB, double &tempoSequencial ){
-    if(colunaA == linhaB){
-        double  comeco, fim, execucao;
-        iniciarZerada(matrixC, linhaA, colunaB);
-        comeco = omp_get_wtime();
-        for (int i = 0; i < linhaA; i++) {
-            for (int j = 0; j < colunaB; j++) {
-                for (int k = 0; k < colunaA; k++) {
-                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
-                }
+    double  comeco, fim, execucao;
+    iniciarZerada(matrixC, linhaA, colunaB);
+    comeco = omp_get_wtime();
+    for (int i = 0; i < linhaA; i++) {
+        for (int j = 0; j < colunaB; j++) {
+            int local_sum = 0;
+            for (int k = 0; k < colunaA; k++) {
+                local_sum += matrixA[i][k] * matrixB[k][j];
+                //matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
             }
+            matrixC[i][j] = local_sum;
         }
-        fim = omp_get_wtime();
-        tempoSequencial = fim - comeco; 
-    }else{
-        cout << "nao atende aos requisitos para realizar a multiplicacao" << endl;
     }
+    fim = omp_get_wtime();
+    tempoSequencial = fim - comeco; 
 }
 
 void multiplicacaoMatrixParallel(int **matrixA, int **matrixB, int **matrixC, int linhaA, int colunaA, int linhaB, int colunaB, double &tempoParalelizado ){
-    if(colunaA == linhaB){
-        double  comeco, fim, execucao;
-        #pragma omp parallel for
-        iniciarZerada(matrixC, linhaA, colunaB);
-        comeco = omp_get_wtime();
-        #pragma omp parallel for
-        for (int i = 0; i < linhaA; i++) {
-            for (int j = 0; j < colunaA; j++) {
-                for (int k = 0; k < colunaA; k++) {
-                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
-                }
+    double  comeco, fim, execucao;
+    //omp_set_num_threads(12);
+    //#pragma omp parallel num_threads(12); 
+    
+    iniciarZerada(matrixC, linhaA, colunaB);
+    comeco = omp_get_wtime();
+    #pragma omp parallel for
+    for (int i = 0; i < linhaA; i++) {
+        for (int j = 0; j < colunaB; j++) {
+            int local_sum = 0;
+            for (int k = 0; k < colunaA; k++) {
+                local_sum += matrixA[i][k] * matrixB[k][j];
+                //matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
             }
+            matrixC[i][j] = local_sum;
         }
-        fim = omp_get_wtime();
-        tempoParalelizado = fim - comeco;
-    }else{
-        cout << "nao atende aos requisitos para realizar a multiplicacao" << endl;
     }
+    fim = omp_get_wtime();
+    tempoParalelizado = fim - comeco;
 }
 
 void iniciarZerada(int** matrix, int linha, int coluna){
@@ -135,3 +151,4 @@ void iniciarZerada(int** matrix, int linha, int coluna){
         }
     }
 }
+
